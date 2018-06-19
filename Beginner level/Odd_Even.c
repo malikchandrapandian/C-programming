e1cf01a7a0297eb1c190e337c9374381a6cc0f8b
malikyadav <38652695+malikyadav@users.noreply.github.com>
2018-06-19 10:51:35 +0000

    Create Odd_Even.c

---
+++ Beginner level/Odd_Even.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  scanf("%d",&a);
+  if(1<=a<=100000)
+  {
+    if(a%2==0)
+    {
+     printf("Even");
+    }
+    else
+    {
+      printf("Odd");
+    }
+  }
+  else
+    {
+      printf("invalid");
+    }
+  
+  return 0;
+}
