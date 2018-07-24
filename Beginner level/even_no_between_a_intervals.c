50d52f64b72951f2e04535f60f4e80f195bafe76
malikyadav <38652695+malikyadav@users.noreply.github.com>
2018-07-24 10:01:44 +0000

    Create even_no_between_a_intervals.c

---
+++ Beginner level/even_no_between_a_intervals.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+int main()
+{
+
+           int f1,f2,rem,i;
+           scanf("%d",&f1);
+           scanf("%d",&f2);
+           for(i=f1; i<=f2; ++i)
+           {
+             rem = i % 2;
+             if(rem == 0)
+              printf("\n  %d",i);
+           }
+} 
+
+ 
