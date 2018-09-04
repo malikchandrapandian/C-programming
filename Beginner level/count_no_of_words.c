fd6e1ea09f1fdc002baa0c37e7e8d247e9899510
malikyadav <38652695+malikyadav@users.noreply.github.com>
2018-09-04 09:47:07 +0000

    Create count_no_of_words.c

---
+++ Beginner level/count_no_of_words.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  char str[50];
+  int i,count=0;
+  gets(str);   
+  for(i=0;str[i];i++)
+       if(str[i] == ' ')
+           count++;            
+  printf("%d",count);
+}
